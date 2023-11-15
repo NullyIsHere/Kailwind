9cb23e669d91eb0f77885c04c47a86ccaa869c4c
NullyIsHere <140397732+NullyIsHere@users.noreply.github.com>
2023-11-15 12:59:48 +0000

    Initial commit

---
+++ kailwind.js
@@ -0,0 +1,11 @@
+console.log("Injecting styles.")
+
+document.head.insertAdjacentHTML(
+  'beforeend',
+  `
+  <!-- Injected by KailWind -->
+  <link href="https://kailwind.nullyih.repl.co/kw.css" rel="stylesheet" type="text/css" />
+  <!-- Injected by KailWind -->
+ 
+`,
+);
